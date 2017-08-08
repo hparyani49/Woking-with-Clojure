60f41dbbee3560923ee2bf6b7705d94ee3e59795
Hitesh <paryanihitesh49@gmail.com>
2017-08-08 17:48:19 +0530

    Completed Spiral game and added midje sample 'quux'

--- spiral-run/src/cljs/spiral_run/core.cljs
+++ spiral-run/src/cljs/spiral_run/core.cljs
@@ -21,33 +21,42 @@
                    :color-arr []}))
 
 (def dice (r/atom 0))
+(def size (r/atom 6))
+
+(defn get-pos-a [val]
+  (let [i (quot (:a-pos @game) 10)
+        j (rem (:a-pos @game) 10)]
+    (+ (* i @size) j)))
+
+(defn get-pos-b [val]
+  (let [i (quot (:b-pos @game) 10)
+        j (rem (:b-pos @game) 10)]
+    (+ (* i @size) j)))
 
 (defn make-move
   "Pushes the player to new position"
   [score]
-  (.log js/console (type (:color-arr @game)))
   (if (zero? (mod (:turn @game) 2))
     (do
       (reset! dice score)
-      (let [arz (doall (drop score (:a-path @game)))]
-        (.log js/console arz)
-        (swap! game assoc-in [:a-pos] (first (first arz)))
-        (swap! game assoc-in [:a-path] arz))
-      (let [i (quot (:a-pos @game) 10)
-            j (rem (:a-pos @game) 10)
-            merged-idx (+ (* i 6) j)]
-        (swap! game assoc-in [:color-arr merged-idx] [0 "blue"]))
-      (swap! game update-in [:turn] inc)
-      (.log js/console "MADE MOVE A"))
+      (swap! game assoc-in [:color-arr (get-pos-a (:a-pos @game))] [0 "yellow"])
+      (swap! game assoc-in [:a-path] (drop score (:a-path @game)))
+      (swap! game assoc-in [:a-pos] (first (first (:a-path @game))))
+      (if (empty? (:a-path @game))
+        (do (js/alert "A won")
+            (.reload js/location true))
+        (swap! game assoc-in [:color-arr (get-pos-a (:a-pos @game))] [0 "blue"]))
+      (swap! game update-in [:turn] inc))
     (do
       (reset! dice score)
+      (swap! game assoc-in [:color-arr (get-pos-b (:b-pos @game))] [0 "red"])
       (swap! game assoc :b-pos (first (first (drop score (:b-path @game)))))
       (swap! game assoc :b-path (drop score (:b-path @game)))
-      (let [i (quot (:b-pos @game) 10)
-            j (rem (:b-pos @game) 10)]
-        (swap! game assoc-in [:color-arr (+ (* i 6) j)] [0 "cyan"]))
-      (swap! game update-in [:turn] inc)
-      (.log js/console "MADE MOVE A"))))
+      (if (empty? (:b-path @game))
+        (do (js/alert "B won")
+            (.reload js/location true))
+        (swap! game assoc-in [:color-arr (get-pos-b (:b-pos @game))] [0 "cyan"]))
+      (swap! game update-in [:turn] inc))))
 
 
 (defn get-color
@@ -55,7 +64,7 @@
   [i j]
   (if (empty? (:color-arr @game ))
     "yellow"
-    (second (nth (:color-arr @game) (+ (* i 6) j)))))
+    (second (nth (:color-arr @game) (+ (* i @size) j)))))
 
 (defn travel
   "Travelling the array to find path of a and b"
@@ -64,27 +73,27 @@
     (flatten arr)
     (let [[x y] (:curr @game)]
       (condp = (:dir @game)
-        "r" (let [newa (conj arr (for [j (range y (- 6 y))]
+        "r" (let [newa (conj arr (for [j (range y (- @size y))]
                                    (get-in @game [:board x j])))]
-              (swap! game assoc-in [:curr] [(inc x) (dec (- 6 y))])
+              (swap! game assoc-in [:curr] [(inc x) (dec (- @size y))])
               (swap! game assoc-in [:dir] "d")
               (travel newa (dec counter)))
 
-        "d" (let [newa (conj arr (for [i (range x (- 6 x))]
+        "d" (let [newa (conj arr (for [i (range x (- @size x))]
                                    (get-in @game [:board i y])))]
-              (swap! game assoc-in [:curr] [(dec (- 6 x)) (dec y)])
+              (swap! game assoc-in [:curr] [(dec (- @size x)) (dec y)])
               (swap! game assoc-in [:dir] "l")
               (travel newa (dec counter)))
 
-        "l" (let [newa  (conj arr (for [j (reverse (range (- 6 (inc y)) (inc y)))]
+        "l" (let [newa  (conj arr (for [j (reverse (range (- @size (inc y)) (inc y)))]
                                     (get-in @game [:board x j])))]
-              (swap! game assoc-in [:curr] [(dec x) (- 6 (inc y))])
+              (swap! game assoc-in [:curr] [(dec x) (- @size (inc y))])
               (swap! game assoc-in [:dir] "u")
               (travel newa (dec counter)))
 
-        "u" (let [newa (conj arr (for [i (reverse (range (- 6 (inc x)) (inc x)))]
+        "u" (let [newa (conj arr (for [i (reverse (range (- @size (inc x)) (inc x)))]
                                    (get-in @game [:board i y])))]
-              (swap! game assoc-in [:curr] [(- 6 (inc x)) (inc y)])
+              (swap! game assoc-in [:curr] [(- @size (inc x)) (inc y)])
               (swap! game assoc-in [:dir] "r")
               (travel newa (dec counter)))))))
 
@@ -92,28 +101,25 @@
 (defn get-paths
   "Gets paths for a and b"
   []
-  (swap! game assoc :a-path (travel [] 5))
+  (swap! game assoc :a-path (travel [] (- @size 1)))
   (swap! game assoc :a-path (doall (mapv (fn [x] [x "yellow"]) (:a-path @game))))
-  (swap! game assoc :curr [5 5] :dir "l")
-  (swap! game assoc :b-path (travel [] 5))
+  (swap! game assoc :curr [(- @size 1) (- @size 1)] :dir "l")
+  (swap! game assoc :b-path (travel [] (- @size 1)))
   (swap! game assoc :b-path (doall (mapv (fn [x] [x "red"]) (:b-path @game))))
-  (swap! game assoc :color-arr  (into [] (sort (into (:a-path @game) (:b-path @game)))
-                                      ) ))
+  (swap! game assoc :color-arr  (into [] (sort (into (:a-path @game) (:b-path @game))))))
 
-(defn show []
-  (.log js/console (clj->js (:color-arr @game)  ) ))
 
 (defn home-page []
-  [:div [:table
-         [:tbody
-          (doall
-           (for [i (range 6)]
-             ^{:key i} [:tr
-                        (doall
-                         (for [j (range 6)]
-                           ^{:key j} [:td {:style {:background-color (get-color i j)}}]))]))]]
+  [:div [:h2 "Welcome to espiral game"] [:table
+            [:tbody
+             (doall
+              (for [i (range @size)]
+                ^{:key i} [:tr
+                           (doall
+                            (for [j (range @size)]
+                              ^{:key j} [:td {:style {:background-color (get-color i j)}}]))]))]]
    [:button {:on-click #(get-paths)} "Start"]
-   [:button {:on-click #(make-move (rand-int 7)) } @dice]])
+   [:button {:on-click #(make-move (rand-int 7))} @dice]])
 
 
 
